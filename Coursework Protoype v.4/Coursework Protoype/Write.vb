33baa0cf68ad5eb969064667a596ff5c2c3329b8
Joshua Beighton <83010264+jlbturbo@users.noreply.github.com>
2023-02-02 16:02:48 +0000

    Write done so far - code will change so I am leaving the stuff that will

--- Coursework Protoype v.4/Coursework Protoype/Write.vb
+++ Coursework Protoype v.4/Coursework Protoype/Write.vb
@@ -1,10 +1,16 @@
 ﻿Public Class Write
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_AddOrder.Click
+        'create new order to store all of the 
         Dim o As New Order
+        'set the ID to equal the amount of orders plus one
         o.id = AllOrders.Count
+        'set the customer to the value of the text box for customers
         o.customer = txt_cust.Text
+        'set the due date to the datetimepicker value
         o.due = date_due.Value
+        'add o to orders
         AllOrders.Add(o)
+        
         Dim os As New orderStock
         Dim s As New Stock
         s.ID = AllStock.Count
@@ -84,7 +90,7 @@
             AllStock.Add(s)
         Next
 
-
+        'clear all of the values
         txt_Case.Clear()
         txt_CPU.Clear()
         txt_cust.Clear()
@@ -95,95 +101,25 @@
         txt_sto.Clear()
         lst_sto.Items.Clear()
 
+        'write all of the changes to the files
         FileHandler.writeOrder()
         FileHandler.writeOrderStock()
         FileHandler.writeStockFile()
     End Sub
 
+    'when the button to add a storage device is pressed
     Private Sub btn_AddStorage_Click(sender As Object, e As EventArgs) Handles btn_AddStorage.Click
+        'add whats in the temporary text box to the list box, clear the text box and select it
         lst_sto.Items.Add(txt_sto.Text)
         txt_sto.Clear()
         txt_sto.Select()
     End Sub
 
-    Private Sub btn_navi_Click(sender As Object, e As EventArgs) Handles btn_navi.Click
+    Private Sub btn_navi_Click(sender As Object, e As EventArgs) Handles btn_navi.Click#
+        'go back to the main database
         Me.Hide()
         seeOrders.Show()
     End Sub
 
-    Private Sub date_due_ValueChanged(sender As Object, e As EventArgs) Handles date_due.ValueChanged
-
-    End Sub
-
-    Private Sub lst_sto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lst_sto.SelectedIndexChanged
-
-    End Sub
-
-    Private Sub txt_sto_TextChanged(sender As Object, e As EventArgs) Handles txt_sto.TextChanged
-
-    End Sub
-
-    Private Sub txt_motherboard_TextChanged(sender As Object, e As EventArgs) Handles txt_motherboard.TextChanged
-
-    End Sub
-
-    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
-
-    End Sub
-
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
-
-    End Sub
-
-    Private Sub txt_cust_TextChanged(sender As Object, e As EventArgs) Handles txt_cust.TextChanged
-
-    End Sub
-
-    Private Sub txt_Case_TextChanged(sender As Object, e As EventArgs) Handles txt_Case.TextChanged
-
-    End Sub
-
-    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
-
-    End Sub
-
-    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
-
-    End Sub
-
-    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
-
-    End Sub
-
-    Private Sub txt_PSU_TextChanged(sender As Object, e As EventArgs) Handles txt_PSU.TextChanged
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub txt_RAM_TextChanged(sender As Object, e As EventArgs) Handles txt_RAM.TextChanged
-
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
-
-    Private Sub txt_GPU_TextChanged(sender As Object, e As EventArgs) Handles txt_GPU.TextChanged
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub txt_CPU_TextChanged(sender As Object, e As EventArgs) Handles txt_CPU.TextChanged
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
+    
 End Class
