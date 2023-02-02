1f98b17ebdf4b58f3b9c50f899bffb3954428430
Joshua Beighton <83010264+jlbturbo@users.noreply.github.com>
2023-02-02 16:06:17 +0000

    write stock commented

--- Coursework Protoype v.4/Coursework Protoype/WriteStock.vb
+++ Coursework Protoype v.4/Coursework Protoype/WriteStock.vb
@@ -2,11 +2,17 @@
 
 
     Private Sub btn_AddOrder_Click(sender As Object, e As EventArgs) Handles btn_AddOrder.Click
+        'if the user has selected a type
         If cmb_type.SelectedIndex >= 0 Then
+            'if the user has entered a part
             If txt_part.Text.Length > 0 Then
+                'if the user has entered a price
                 If txt_price.Text.Length > 0 Then
+                    'create a new stock item
                     Dim s As New Stock
+                    'set the stock item's ID to the amount of stock plus one
                     s.ID = AllStock.Count
+                    'set the rest of the data according to what the user has entered
                     s.ordered = date_ordered.Value
                     s.Due = date_due.Value
                     s.cost = Convert.ToInt32(txt_price.Text)
@@ -15,24 +21,28 @@
                     s.part = txt_part.Text
                     AllStock.Add(s)
 
-
+                    'clear the UI elements
                     txt_part.Clear()
                     txt_price.Clear()
                     chk_arrived.Checked = False
                     cmb_type.SelectedIndex = -1
                     cmb_type.Text = ""
                 Else
+                    'ask the user to enter a price
                     MsgBox("Please Enter a Price")
                 End If
             Else
+                'ask the user to enter a part
                 MsgBox("Please Enter a Part")
             End If
         Else
+            'ask the user to enter a type
             MsgBox("Please Select A Type")
         End If
 
     End Sub
 
+    'go back to the main database
     Private Sub btn_navi_Click(sender As Object, e As EventArgs) Handles btn_navi.Click
         Me.Close()
         seeStock.Show()
