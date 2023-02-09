d9e8cddf560a87e25bf3021f9ba791622a78c2d3
Joshua Beighton <83010264+jlbturbo@users.noreply.github.com>
2023-02-09 14:56:44 +0000

    few comments

--- Coursework Protoype v.4/Coursework Protoype/Form1.vb
+++ Coursework Protoype v.4/Coursework Protoype/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+    'set up the data
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         readOrders()
         readOrderStock()
@@ -9,22 +10,30 @@
         Me.Hide()
     End Sub
 
+    'handle login
     Private Sub btn_login_Click(sender As Object, e As EventArgs) Handles btn_login.Click
-
+        'store whether the username/password has been found
         Dim found As Boolean = False
+        'loop through all users
         For i = 0 To AllUsers.Count - 1
+            'if the username and password both match
             If txt_username.Text = AllUsers(i).username And txt_pass.Text = AllUsers(i).password Then
+                'show and setup navigation form and hide the login form
                 Navigation.Show()
                 Navigation.setup(AllUsers(i).permission)
                 Me.Hide()
                 found = True
             End If
         Next
+        'if the credentials are not in the system
         If found = False Then
             MsgBox("Please enter valid login info")
         End If
+        'reset the form
         reset()
     End Sub
+
+    'reset the login form and select the username text box 
     Sub reset()
         txt_pass.Clear()
         txt_username.Clear()
--- Coursework Protoype v.4/Coursework Protoype/Navigation.vb
+++ Coursework Protoype v.4/Coursework Protoype/Navigation.vb
@@ -1,38 +1,46 @@
-﻿Public Class Navigation
-    Private Sub Navigation_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+Public Class Navigation
 
-    End Sub
+    
     Sub setup(type As Char)
+        'show all the text boxes
         btn_cust.Show()
         btn_logs.Show()
         btn_orders.Show()
         btn_parts.Show()
         btn_sales.Show()
         btn_stock.Show()
+
+        'if the user is an accountant
         If type = "A" Then
+            'hide all irrelavant buttons
             btn_stock.Hide()
             btn_parts.Hide()
             btn_orders.Hide()
             btn_logs.Hide()
             btn_cust.Hide()
             btn_staff.Hide()
+        'if user is staff member
         ElseIf type = "S" Then
+            'hide irrelavant buttons
             btn_logs.Hide()
             btn_staff.Hide()
             btn_sales.Hide()
         End If
     End Sub
 
+    'navigate to the orders for when the orders button is clicked
     Private Sub btn_orders_Click(sender As Object, e As EventArgs) Handles btn_orders.Click
         seeOrders.Show()
         Me.Hide()
     End Sub
 
+    'navigate to the stock form when the button is clicked
     Private Sub btn_stock_Click(sender As Object, e As EventArgs) Handles btn_stock.Click
         seeStock.Show()
         Me.Hide()
     End Sub
 
+    'go back to the login form when the user signs out
     Private Sub btn_logout_Click(sender As Object, e As EventArgs) Handles btn_logout.Click
         Me.Hide()
         Form1.Show()
