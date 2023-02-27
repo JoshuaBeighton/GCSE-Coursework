7e43ea77981676da56ddbfd56cd906fa419aecc3
Joshua Beighton <83010264+jlbturbo@users.noreply.github.com>
2023-02-27 12:06:45 +0000

    done commenting everything so far!

--- Coursework Protoype v.4/Coursework Protoype/Write.vb
+++ Coursework Protoype v.4/Coursework Protoype/Write.vb
@@ -1,13 +1,21 @@
 ﻿Public Class Write
+    'store the ID of the stock item storing the CPU
     Dim sCPU As Integer
+    'store the ID of the stock item storing the RAM
     Dim sRAM As Integer
+    'store the ID of the stock item storing the GPU
     Dim sGPU As Integer
+    'store the ID of the stock item storing the Motherboard
     Dim sMoba As Integer
+    'store the ID of the stock item storing the Case
     Dim sCase As Integer
+    'store the ID of the stock item storing the PSU
     Dim sPSU As Integer
+    'store the IDs of the stock items storing the storage
     Dim sSto As New List(Of Integer)
+
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_AddOrder.Click
-        'create new order to store all of the 
+        'create new order to store all of the data
         Dim o As New Order
         'set the ID to equal the amount of orders plus one
         o.id = AllOrders.Count
@@ -18,45 +26,61 @@
         'add o to orders
         AllOrders.Add(o)
 
+        'create new orderstock item
         Dim os As New orderStock
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
+        'set its order ID to that of the Order - this will not need to be changed again as it is the same for all orderstock items for the same order
         os.order = o.id
+        'set its stock ID to that of the CPU
         os.stock = sCPU
+        'add the orderstock item
         AllOrderStock.Add(os)
 
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
-        os.order = o.id
+        'set its stock ID to that of the GPU
         os.stock = sGPU
+        'add the orderstock item
         AllOrderStock.Add(os)
 
 
-
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
-        os.order = o.id
+        'set its stock ID to that of the RAM
         os.stock = sRAM
+        'add the orderstock item
         AllOrderStock.Add(os)
 
 
-
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
-        os.order = o.id
+        'set its stock ID to that of the PSU
         os.stock = sPSU
+        'add the orderstock item
         AllOrderStock.Add(os)
 
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
-        os.order = o.id
+        'set its stock ID to that of the case
         os.stock = sCase
+        'add the orderstock item
         AllOrderStock.Add(os)
 
+        'set its ID to the count of orderstock items
         os.ID = AllOrderStock.Count
-        os.order = o.id
+        'set its stock ID to that of the motherboard
         os.stock = sMoba
+        'add the orderstock item
         AllOrderStock.Add(os)
 
-
+        'loop through the storage devices
         For i = 0 To sSto.Count - 1
+            'set the orderstock ID to the count of orderstock items
             os.ID = AllOrderStock.Count
+            'set its stock ID to that of the current storage device
             os.stock = sSto(i)
+            'add the orderstock item
             AllOrderStock.Add(os)
         Next
 
@@ -91,121 +115,188 @@
         seeOrders.Show()
     End Sub
 
+    'on clicking the select CPU button
     Private Sub selCPU_Click(sender As Object, e As EventArgs) Handles selCPU.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select a CPU
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("CPU")
+        'store that we are waiting for a CPU
         type = "CPU"
     End Sub
+    'store what type of item we are waiting to be selected 
     Public type As String
 
+    'on clicking the select RAM button
     Private Sub selRAM_Click(sender As Object, e As EventArgs) Handles selRAM.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select RAM
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("RAM")
+        'store that we are waiting for RAM
         type = "RAM"
     End Sub
 
+    'on clicking the select Case button
     Private Sub selCase_Click(sender As Object, e As EventArgs) Handles selCase.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select a case
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("Case")
+        'store that we are waiting for a case
         type = "Case"
     End Sub
 
+    'on clicking the select GPU button
     Private Sub selGPU_Click(sender As Object, e As EventArgs) Handles selGPU.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select a GPU
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("GPU")
+        'store that we are waiting for a GPU
         type = "GPU"
     End Sub
 
 
-
+    'on clicking the select motherboard button
     Private Sub selMoba_Click(sender As Object, e As EventArgs) Handles selMoba.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select a motherboard
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("Motherboard")
+        'store that we are waiting for a motherboard
         type = "Motherboard"
     End Sub
+
+    'on returning from the see stock page 
     Sub onReturn(index As Integer)
+        'do different things depending on which part we are waiting to get back
         Select Case type
+            'if we're waiting to get a CPU back
             Case "CPU"
+                'hide the button to select and show the text box that shows which item was selected
                 selCPU.Hide()
                 txt_CPU.Show()
+                'store the ID of the selected item
                 sCPU = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_CPU.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get a GPU back
             Case "GPU"
+                'hide the button to select and show the text box that shows which item was selected
                 selGPU.Hide()
                 txt_GPU.Show()
+                'store the ID of the selected item
                 sGPU = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_GPU.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get a PSU back
             Case "PSU"
+                'hide the button to select and show the text box that shows which item was selected
                 selPSU.Hide()
                 txt_PSU.Show()
+                'store the ID of the selected item
                 sPSU = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_PSU.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get a case back
             Case "Case"
+                'hide the button to select and show the text box that shows which item was selected
                 selCase.Hide()
                 txt_Case.Show()
+                'store the ID of the selected item
                 sCase = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_Case.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get a motehrboard back
             Case "Motherboard"
+                'hide the button to select and show the text box that shows which item was selected
                 selMoba.Hide()
                 txt_motherboard.Show()
+                'store the ID of the selected item
                 sMoba = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_motherboard.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get RAM back
             Case "RAM"
+                'hide the button to select and show the text box that shows which item was selected
                 selRAM.Hide()
                 txt_RAM.Show()
+                'store the ID of the selected item
                 sRAM = index
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         txt_RAM.Text = AllStock(i).part
                     End If
                 Next
+            'if we're waiting to get storage back
             Case "Storage"
+                'loop through the stock list
                 For i = 0 To AllStock.Count - 1
+                    'if the selected item is the currently checked item in allstock
                     If index = AllStock(i).ID Then
+                        'set the value of the text box to the name of the part
                         lst_sto.Items.Add(AllStock(i).part)
                     End If
                 Next
-
+                'store the ID of the selected item
                 sSto.Add(index)
         End Select
     End Sub
+
+    'initialise the write page
     Sub init()
+        'hide all of the text boxes that store the values that have been selected
         txt_Case.Hide()
         txt_CPU.Hide()
         txt_GPU.Hide()
         txt_motherboard.Hide()
         txt_PSU.Hide()
         txt_RAM.Hide()
+        'show the buttons that select the items
         selCase.Show()
         selCPU.Show()
         selGPU.Show()
@@ -213,11 +304,16 @@
         selPSU.Show()
         selRAM.Show()
     End Sub
+
+    'on clicking the select PSU button
     Private Sub selPSU_Click(sender As Object, e As EventArgs) Handles selPSU.Click
+        'navigate to the see stock page
         Me.Hide()
         seeStock.Show()
+        'initialise see stock to select a PSU
         seeStock.InitialiseSeeStock()
         seeStock.selectInit("PSU")
+        'store that we are waiting for a PSU
         type = "PSU"
     End Sub
 End Class
